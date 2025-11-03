974561a71045d9c97ef8e4b869643f5b700fd787
Magali179 <malali.moreno@gmail.com>
2025-11-03 17:36:21 -0300

    mis primeros addEvenlisttener

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-var nombre = document.getElementById("nombre").value;
-console.log(nombre);
+// var nombre = document.getElementById("nombre").value;
+// console.log(nombre);
+
+nombre.addEventListener('input', function() {
+        console.log("Input changed:", this.value);
+    });
+
+apellido.addEventListener('input', function() {
+        console.log("Input changed:", this.value);
+    });
